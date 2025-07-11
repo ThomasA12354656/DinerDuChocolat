7a0f0bf1cac1c624d5289f64cd7639c2e46702bf
ThomasA12354656 <thomas@dsamedia.co.uk>
2025-07-11 09:59:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+// Simple script to handle contact form submission
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for reaching out! We will get back to you soon.');
+    this.reset();
+});
